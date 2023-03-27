87f28a19f7a8ef41a3e1af2d2858cfc27fc6d4dd
Peter Žáčik <pezacik@gmail.com>
2023-03-27 10:44:17 +0200

    Canary change

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func Helloworld(g *gin.Context) {
 	sleep := rand.Intn(100)
 
 	time.Sleep(time.Millisecond * time.Duration(sleep))
-	g.JSON(http.StatusOK, "helloworld")
+	g.JSON(http.StatusOK, "helloworld 2")
 }
 
 // @title			Example API
